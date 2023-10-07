136d24e592eb423929a06f012a1dfd6143d4b9b4
Tiger Lu <tigerlu@vpn172022151246.its.yale.internal>
2023-10-07 13:44:16 -0400

    around p1 now

--- examples/migration_testing/problem.c
+++ examples/migration_testing/problem.c
@@ -26,9 +26,9 @@ double inv_alignment_ts = 1./(1e6 * M_PI * 2.);
 
 char title1[100] = "output_orbits_107.txt";
 char title2[100] = "output_spins_107.txt";
-char title3[100] = "output_tp_bvec_107_nd_ols_p2.txt";
+char title3[100] = "output_tp_bvec_107_nd_ols.txt";
 
-const int p = 2;
+const int p = 1;
 
 void derivatives(struct reb_ode* const ode, double* const yDot, const double* const y, const double t){
     // From Zanazzi & Lai 2017
